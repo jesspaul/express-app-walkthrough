d49df70d82091f3d3f11a33651dff33424888d8c
Jessica Paul <paul.jessica95@gmail.com>
2021-03-14 00:15:09 -0500

    add click function to toggle code sections

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ $('body').on('click', '.direction', toggle);
 $('body').find('.code').css('display', 'none');
 
 function toggle(evt) {
-    $(evt.target).next().toggle();
+    $(evt.target).next().fadeToggle();
 }
