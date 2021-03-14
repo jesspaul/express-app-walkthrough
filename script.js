bd2ca6a120f26d925f851c466a02d2e1e5d73e2d
Jessica Paul <paul.jessica95@gmail.com>
2021-03-14 13:21:17 -0400

    add expand/collapse all buttons

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+// cached elements
+const $expand = $('#expand');
+const $collapse = $('#collapse');
+
+// event handlers
 $('body').on('click', '.direction', toggle);
+$expand.on('click', expandAll);
+$collapse.on('click', collapseAll);
 
-$('body').find('.code').css('display', 'none');
-
+// functions
 function toggle(evt) {
     $(evt.target).next().fadeToggle();
+    if ($(evt.target).html().indexOf('caret-down') !== -1) {
+        $(evt.target).children().remove();
+        $(evt.target).append(' <i class="fa fa-caret-up"></i>');
+    } else if ($(evt.target).html().indexOf('caret-up') !== -1) {
+        $(evt.target).children().remove();
+        $(evt.target).append(' <i class="fa fa-caret-down"></i>');
+    }
 }
+
+function expandAll() {
+    $('body').find('.code').fadeIn();
+    $('body').find('.direction').children().remove();
+    $('body').find('.direction').append(' <i class="fa fa-caret-up"></i>');
+}
+
+function collapseAll() {
+    $('body').find('.code').fadeOut();
+    $('body').find('.direction').children().remove();
+    $('body').find('.direction').append(' <i class="fa fa-caret-down"></i>');
+}
+
+// execute on page load
+$('body').find('.code').css('display', 'none');
+$('body').find('.direction').append(' <i class="fa fa-caret-down"></i>');
